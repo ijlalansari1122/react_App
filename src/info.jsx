6204f0de5326665d3bbebabdb534a10f95c47021
ijlalansari1122 <ansariijlal90@gmail.com>
2022-02-25 13:17:28 +0500

    first ui interface for my reat app

---
+++ src/info.jsx
@@ -0,0 +1,33 @@
+// import React from "react";
+
+
+function Number() {
+
+
+return(
+
+<div className="menu">
+    <div className="logo">
+
+
+</div>    <nav>
+     <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Sign</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Resources</a></li>
+            </ul>
+    </nav>
+
+
+</div>
+
+
+    );
+    
+}
+export default Number;
+
+
